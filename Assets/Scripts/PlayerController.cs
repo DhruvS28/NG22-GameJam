dccea0cf70f2c82b62e0ec67bbfd9ac7af700555
Dhruv Sharma <dhs404@usask.ca>
2022-05-14 16:15:15 -0400

    Basic mine functionality

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,11 +21,44 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+          Debug.Log("Testing");
+        }
+
+
         move.y = Input.GetAxisRaw("Vertical");
         move.x = Input.GetAxisRaw("Horizontal");
 
         rb.MovePosition(rb.position + move.normalized * speed * Time.fixedDeltaTime);
 
-
     }
+
+
+    void OnTriggerEnter2D(Collider2D collider) 
+    {
+        Debug.Log(collider);
+        StartCoroutine("CountDown");
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision);
+        StopAllCoroutines();
+        Debug.Log("Boom on collision");
+    }
+
+    IEnumerator CountDown()
+    {
+        for(int i = 3; i > 0; i--)
+        {
+            Debug.Log(i);
+            yield return new WaitForSeconds(1f);
+        }
+
+        Debug.Log("Boom by time");
+    }
+
+
 }
